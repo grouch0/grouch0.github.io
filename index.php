08b523876b527162f0501d3848801cf130b16bf7
Mike Steffan <mrsteffan1@yahoo.com>
2016-09-28 17:47:59 -0500

    Add php nad image

---
+++ index.php
@@ -0,0 +1,15 @@
+<html>
+<head>
+<title>
+grouch0's 1st github page
+</title>
+</head>
+<body>
+<h1>Hello, World.</h1>
+<?php 
+for ($x = 0; $x <= 100; $x++) {
+    echo "The number is: $x <br>";
+} 
+?>
+</body>
+</html>
